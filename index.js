eacec065c08f7e5af831a7bc770facd940d1e8f3
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-05-03 21:30:16 +0200

    added sockets for messages (currently broadcasting to user)

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const socket = require('socket.io')
 require('dotenv').config()
 
 require('./startup/cors')(app)
@@ -10,5 +11,15 @@ const port = process.env.PORT || 8000
 const server = app.listen(port, () =>
   console.log(`Listening on port ${port}...`)
 )
+
+const io = socket(server)
+io.on('connection', socket => {
+  console.log(`Made socket connection ${socket.id}`)
+
+  socket.on('chatMessageSent', message => {
+    console.log({ message })
+    socket.broadcast.emit('loadMessage', message)
+  })
+})
 
 module.exports = server
