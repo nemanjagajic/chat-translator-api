dfee896c10a443bca1650ce6b260497a7e4dae06
nemanjagajic <nemanja.gajicru96@gmail.com>
2020-08-19 19:10:23 +0200

    moved formidable to only one (updateUser) endpoint

--- routes/auth.js
+++ routes/auth.js
@@ -2,12 +2,13 @@ const express = require('express')
 const router = express.Router()
 const usersController = require('../controllers/usersController')
 const authMiddleware = require('../middleware/auth')
+const formidable = require('express-formidable')
 
 router.post('/register', usersController.register)
 router.post('/login', usersController.login)
 router.post('/loginWithGoogle', usersController.loginWithGoogle)
 router.get('/me', authMiddleware, usersController.me)
 router.post('/registerNotificationToken', authMiddleware, usersController.registerNotificationToken)
-router.post('/updateUser', authMiddleware, usersController.updateUser)
+router.post('/updateUser', [authMiddleware, formidable()], usersController.updateUser)
 
 module.exports = router
